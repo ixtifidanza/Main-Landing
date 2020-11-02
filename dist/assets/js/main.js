ce0222f5a4e8ff47e4da4c9e59040895cb95163e
ixtifidanza <ixtifidanza@gmail.com>
2020-11-02 15:14:21 +0500

    adaptive

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,30 +1,41 @@
-var swiper = new Swiper('.swiper-container', {
-    // slidesPerView: 1,
-    // spaceBetween: 20,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    // loopFillGroupWithBlank: true,    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+$(function () {
 
+$('[data-scroll]').on('click', function(event) {
+    event.preventDefault(); 
+
+    var elementId = $(this).data('scroll');
+    
+    var elementOffset = $(elementId).offset().top;
+
+    $('html, body').animate({
+        scrollTop: elementOffset -70
+    }, 2000);
+});
+    
+    
+    
+    
+    
+    
+    var swiper = new Swiper('.swiper-container', {
+       
+        slidesPerView: 2,
+        spaceBetween: 0,
+        loop: true,
+        
+        navigation: {
+          nextEl: '.next-btn',
+          prevEl: '.prev-btn',
+        },
+    });
+    
+    
+ 
+    
+      $('.menu-btn').on('click', function() {
+        $(this).toggleClass('menu-btn_active');
+        $('.menu-nav').toggleClass('menu-nav_active');
+      });
     
 });
-
-
-  $ (function () {
-
-
-    let navToggle = $('#navToggle');
-
-
-    
-  navToggle.on('click', function(event) {
-  event.preventDefault();
-  nav.toggleClass('show');
-
-    
-  });
 
